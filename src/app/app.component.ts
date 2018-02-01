d24c04ebe93791c202903bf3ab769ed4850c5cb0
Kenneth Gathuru <kgathuru@gmail.com>
2018-02-01 17:06:13 +0300

    Link to new DICOM Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {CornerstoneService} from './services/cornerstone.service';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CornerstoneService } from './services/cornerstone.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
 
 
   imageData: any;
@@ -18,9 +18,9 @@ export class AppComponent implements  OnInit {
 
   ngOnInit() {
 
-    for (const i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11 , 12 , 13, 14 ,15 , 16 , 17 , 18]) {
-      this.csS.fetchDicomImage(`http://localhost:4200/assets/dicom/im${i}`)
-        .subscribe(res =>  this.imageData = res);
+    for (const i of ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18']) {
+      this.csS.fetchDicomImage(`http://localhost:4200/assets/dicom/CT0000${i}.dcm`)
+        .subscribe(res => this.imageData = res);
     }
 
 
